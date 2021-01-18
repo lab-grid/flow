cdb2eec15590ad15571033ded22ced088b182c6a
Robert Chu <allegrormc@gmail.com>
2021-01-17 20:31:40 -0800

    Adds importing indicator to import modal.

--- web/src/components/TableImportModal.tsx
+++ web/src/components/TableImportModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Button, Form, Modal, Spinner } from 'react-bootstrap';
 import { useRecoilCallback } from 'recoil';
 import { useModalTracking } from '../analytics';
 import { labflowOptions } from '../config';
@@ -18,16 +18,22 @@ export interface TableImportModalProps<T={[field: string]: any}> {
 
 export function TableImportModal<T={[field: string]: any}>(props: TableImportModalProps<T>) {
     useModalTracking("table-import");
+    const [importing, setImporting] = useState(false);
     const [paramValues, setParamValues] = useState<{[param: string]: string}>({});
 
     const importTable = useRecoilCallback(({ snapshot }) => async () => {
-        let url = props.url;
-        for (const [param, value] of Object.entries(paramValues)) {
-            url = url.replaceAll(`:${param}`, value);
+        setImporting(true);
+        try {
+            let url = props.url;
+            for (const [param, value] of Object.entries(paramValues)) {
+                url = url.replaceAll(`:${param}`, value);
+            }
+            const { auth0Client } = await snapshot.getPromise(auth0State);
+            const { results, attachments } = await externalFetch<BlockResultsImport<T>>(labflowOptions, () => auth0Client, props.method, url);
+            props.setTable(results || [], attachments || {});
+        } finally {
+            setImporting(false);
         }
-        const { auth0Client } = await snapshot.getPromise(auth0State);
-        const { results, attachments } = await externalFetch<BlockResultsImport<T>>(labflowOptions, () => auth0Client, props.method, url);
-        props.setTable(results || [], attachments || {});
     });
 
     return <Modal show={props.show} onHide={props.setShow} size="lg" animation={false}>
@@ -41,6 +47,7 @@ export function TableImportModal<T={[field: string]: any}>(props: TableImportMod
                         <Form.Label>{param}</Form.Label>
                         <Form.Control
                             type="text"
+                            disabled={importing}
                             value={paramValues[param] || ''}
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                                 const newParamValues = { ...paramValues };
@@ -51,12 +58,30 @@ export function TableImportModal<T={[field: string]: any}>(props: TableImportMod
                     </Form.Group>
                 )}
             </Form>
+            {
+                importing && <div className="d-flex mt-5">
+                    <div className="col"></div>
+                    <div className="col-auto mr-3 my-auto">
+                        Importing...
+                    </div>
+                    <div className="col-auto">
+                        <Spinner
+                            as="span"
+                            animation="border"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                    </div>
+                    <div className="col"></div>
+                </div>
+            }
         </Modal.Body>
         <Modal.Footer>
-            <Button variant="primary" onClick={importTable}>
+            <Button variant="primary" onClick={importTable} disabled={importing}>
                 Import
             </Button>
-            <Button variant="secondary" onClick={() => props.setShow(false)}>
+            <Button variant="secondary" onClick={() => props.setShow(false)} disabled={importing}>
                 Close
             </Button>
         </Modal.Footer>
